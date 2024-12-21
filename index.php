4d919f7c7327599b072a1c68dd32240bfb44f3b7
Maryam685 <fa22-bce-020@cuiatk.edu.pk>
2024-12-21 21:23:56 +0500

    Update index.php

--- index.php
+++ index.php
@@ -70,7 +70,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- <link href="../../css/assets/fontawesome/css/all.css" rel="stylesheet" /> -->
+    <link href="../../css/assets/fontawesome/css/all.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha384-dyBrrrlz1f4BZp7nuZVG0EF2gLVdyPe3K1L/ve53U+M1zYQFpRhzYOuFh3roHQII" crossorigin="anonymous" />
     <link rel="stylesheet" href="assets/css/style.css"/>
     <title>Todo List Project</title>
